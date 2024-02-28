1803caddbbe51e57440e09f14eedb558e47af4b1
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-02-28 17:03:32 +0530

    table of any given number . Programm

--- TablesofGivenNUMBER.java
+++ TablesofGivenNUMBER.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 public class TablesofGivenNUMBER {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        for(int i = n; i <=)
+        System.out.print("Enter the number : ");
+        int n = read.nextInt();
+        System.out.println("the table is : ");
+        for(int i =1; i <= 10; i++){
+            System.out.println(n*i);
+        }
     }
 }
